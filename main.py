45ea6f3395bc742073404068ae814bda0d2db3a6
Thomas <gamer.my2@gmail.com>
2025-06-26 17:10:52 +0200

    Asteroids CH2 L3

--- main.py
+++ main.py
@@ -5,10 +5,9 @@ import pygame
 from constants import *
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     
     while True:
         for event in pygame.event.get():
@@ -17,6 +16,8 @@ def main():
                 
         screen.fill("black")
         pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
     
 
 if __name__ == "__main__":
